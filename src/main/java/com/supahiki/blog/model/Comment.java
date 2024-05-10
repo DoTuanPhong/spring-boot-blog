c7e4051d2dce9393dc6220e3467d07673cc89879
DoTuanPhong <phong02468@gmail.com>
2024-05-10 20:04:51 +0700

    Create annotations to connect with MySQL database.

--- src/main/java/com/supahiki/blog/model/Comment.java
+++ src/main/java/com/supahiki/blog/model/Comment.java
@@ -1,4 +1,75 @@
 package com.supahiki.blog.model;
 
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "comment")
 public class Comment {
+    @Column(name = "comment_id")
+    private int commentId;
+    private String content;
+    @Column(name = "create_at")
+    private Date createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public Comment() {
+    }
+
+    public int getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(int commentId) {
+        this.commentId = commentId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Comment(int commentId, String content, Date createdAt, Post post, User user) {
+        this.commentId = commentId;
+        this.content = content;
+        this.createdAt = createdAt;
+        this.post = post;
+        this.user = user;
+    }
 }
--- src/main/java/com/supahiki/blog/model/Post.java
+++ src/main/java/com/supahiki/blog/model/Post.java
@@ -1,31 +1,75 @@
 package com.supahiki.blog.model;
 
+import jakarta.persistence.*;
+
 import java.util.Date;
 
+@Entity
+@Table (name = "post")
 public class Post {
+    @Column(name = "post_id")
     private int postId;
+
     private String title;
     private String content;
+
+    @Column(name = "created_at")
     private Date createdAt;
+    @Column(name = "updated_at")
     private Date updatedAt;
 
-    public Post() {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name ="author_id", nullable = false)
+    private User author;
+
+    public int getPostId() {
+        return postId;
     }
 
-    public Post(int postId, String title, String content) {
+    public void setPostId(int postId) {
         this.postId = postId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
         this.content = content;
     }
 
-    @Override
-    public String toString() {
-        return "Post{" +
-                "postId=" + postId +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                ", createdAt=" + createdAt +
-                ", updatedAt=" + updatedAt +
-                '}';
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public Post() {
     }
 }
