f164ee0395bcd316df457781533293e5ab3c7c91
DoTuanPhong <phong02468@gmail.com>
2024-05-10 20:05:11 +0700

    New model.

--- src/main/java/com/supahiki/blog/model/Tag.java
+++ src/main/java/com/supahiki/blog/model/Tag.java
@@ -1,4 +1,29 @@
 package com.supahiki.blog.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tag")
 public class Tag {
+    @Column(name = "tag_id")
+    private int tagId;
+    private String name;
+
+    public int getTagId() {
+        return tagId;
+    }
+
+    public void setTagId(int tagId) {
+        this.tagId = tagId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
