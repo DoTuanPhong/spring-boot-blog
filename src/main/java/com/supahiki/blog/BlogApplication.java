130f8c1976425d0dec69a086fc7114478e55377f
DoTuanPhong <phong02468@gmail.com>
2024-05-10 18:21:13 +0700

    make some test

--- src/main/java/com/supahiki/blog/BlogApplication.java
+++ src/main/java/com/supahiki/blog/BlogApplication.java
@@ -1,18 +1,28 @@
 package com.supahiki.blog;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.supahiki.blog.model.Post;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
-@SpringBootApplication(exclude = {
-		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
-		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
-)
+@SpringBootApplication
+//		(exclude = {
+//		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+//		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
+//)
+@RestController
 public class BlogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 	}
 
+	@GetMapping("")
+	public List<Post> hello(){
+		return List.of(new Post(1,"Sample Post Title", "Lorem ipsum dolor sit amet, consectetur adipiscing elit."));
+	}
 }
