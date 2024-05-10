d5d2195303a964f9cfc7ae3348fa95763d383a59
DoTuanPhong <phong02468@gmail.com>
2024-05-10 11:10:33 +0700

    Add new dependancy

--- src/main/java/com/supahiki/blog/BlogApplication.java
+++ src/main/java/com/supahiki/blog/BlogApplication.java
@@ -1,9 +1,14 @@
 package com.supahiki.blog;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
+)
 public class BlogApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/supahiki/blog/BlogController.java
@@ -0,0 +1,13 @@
+package com.supahiki.blog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class BlogController {
+ @GetMapping("/blog")
+    public String blogPage(){
+     return "blog";
+ }
+}
