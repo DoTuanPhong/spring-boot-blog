b3fe866cf662043e24100929b5dec65b6693c210
DoTuanPhong <phong02468@gmail.com>
2024-05-14 20:06:36 +0700

    Add new file.

---
+++ src/main/java/com/supahiki/blog/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.supahiki.blog.repository;
+
+import com.supahiki.blog.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByUsername(String username);
+    User finByEmail(String email);
+    List<User> findAllByEnable(boolean enabled);
+}
