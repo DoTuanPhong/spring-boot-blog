1c4501b7bb6a53ee9e5b17d7912b54eef3283498
DoTuanPhong <phong02468@gmail.com>
2024-05-15 20:47:37 +0700

    Using PagedResponse and Dto convert.

--- src/main/java/com/supahiki/blog/service/PostService.java
+++ src/main/java/com/supahiki/blog/service/PostService.java
@@ -1,17 +1,24 @@
 package com.supahiki.blog.service;
 
+import com.supahiki.blog.dto.PostDto;
 import com.supahiki.blog.model.Post;
 import com.supahiki.blog.model.User;
+import com.supahiki.blog.payload.PagedResponse;
 import com.supahiki.blog.repository.PostRepository;
 import com.supahiki.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class PostService {
@@ -21,8 +28,24 @@ public class PostService {
     @Autowired
     private UserRepository userRepository;
 
-    public List<Post> getAllPosts (){
-        return postRepository.findAll();
+    public PagedResponse<PostDto> getAllPosts (
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "12") int size){
+        Pageable pageable = PageRequest.of(page,size);
+        Page<Post> postPage = postRepository.findAll(pageable);
+
+        List<PostDto> content = postPage.getContent().stream()
+                .map(this::convertToDto)
+                .collect(Collectors.toList());
+        return new PagedResponse<>(
+                content,
+                postPage.getNumber(),
+                postPage.getSize(),
+                postPage.getTotalElements(),
+                postPage.getTotalPages(),
+                postPage.isLast()
+        );
+
     }
 
     public List<Post> getPostsByAuthor(String username){
@@ -53,6 +76,16 @@ public class PostService {
     public void deletePost(Integer id){
          Optional<Post> optionalPost = postRepository.findById(id);
          optionalPost.ifPresent(postRepository::delete);
+    }
+
+    private PostDto convertToDto(Post post){
+        PostDto postDto = new PostDto();
+        postDto.setId(post.getPostId());
+        postDto.setTitle(post.getTitle());
+        postDto.setAuthorName(post.getAuthor().getUsername());
+        postDto.setCreatedAt(post.getCreatedAt());
+        postDto.setUpdatedAt(post.getUpdatedAt());
+        return postDto;
     }
 
 
