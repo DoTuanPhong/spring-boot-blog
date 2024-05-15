69a1325710d315d9ae0beb66c170628ef04232db
DoTuanPhong <phong02468@gmail.com>
2024-05-15 20:48:26 +0700

    Remove undefined methods.

--- src/main/java/com/supahiki/blog/repository/UserRepository.java
+++ src/main/java/com/supahiki/blog/repository/UserRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findByUsername(String username);
-    User finByEmail(String email);
-    List<User> findAllByEnable(boolean enabled);
+    /*User finByEmail(String email);
+    List<User> findAllByEnable(boolean enabled);*/
 }
